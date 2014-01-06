ea7f07b3c7ef3ac87b4e7e67d08b6f007e8953f2
Autch <autch@autch.net>
2014-01-06 20:35:08 +0900

    Bootstrap 3.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,29 @@
 $(function() {
-  var update_visibility = function() {
-   
-    $('tr.prog-BS').toggle($('input.hide-ch-type[data-ch-type="BS"]').prop("checked"));
-    $('tr.prog-CS').toggle($('input.hide-ch-type[data-ch-type="CS"]').prop("checked"));
-    $('tr.prog-GR').toggle($('input.hide-ch-type[data-ch-type="GR"]').prop("checked"));
+    var update_visibility = function(e) {
+	$('tr.prog-BS').toggle($('#hide-ch-type input[value="BS"]').prop("checked"));
+	$('tr.prog-CS').toggle($('#hide-ch-type input[value="CS"]').prop("checked"));
+	$('tr.prog-GR').toggle($('#hide-ch-type input[value="GR"]').prop("checked"));
 
-    if($('input.hide-prog-status[data-prog-status="f"]').prop("checked")) {
-      $('tr.f').hide();
-    }
-    if($('input.hide-prog-status[data-prog-status="r"]').prop("checked")) {
-      $('tr.r').hide();
-    }
-  };
+	if($('#hide-prog-status input[value="f"]').prop("checked")) {
+	    $('tr.f').hide();
+	}
+	if($('#hide-prog-status input[value="r"]').prop("checked")) {
+	    $('tr.r').hide();
+	}
+    };
 
-  update_visibility();
+    var initialize_toggle = function(index, elem) {
+	var $this = $(this);
+	if($this.prop("checked")) $this.parent("label").button("toggle");
+    };
 
-  $(document).on("change", "input.hide-prog-status", function(e) {
+    $('#hide-ch-type input').each(initialize_toggle);
+    $('#hide-prog-status input').each(initialize_toggle);
+
+
     update_visibility();
-  });
 
-  $(document).on("change", "input.hide-ch-type", function(e) {
-    update_visibility();
-  });
-
+    $("#hide-prog-status").on("change", "input", update_visibility);
+    $("#hide-ch-type").on("change", "input", update_visibility);
 });
 
